c025a53d88fe9893aba66052ba040c86b51c20d5
UBISOFT-ORG\sdellaoui <slimane.dellaoui@ubisoft.com>
2017-05-24 14:09:48 +0200

    Init Repo
    Setup Unity project

---
+++ Assets/Editor/SoundManagerEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEditor;
+using UnityEngine.Audio;
+
+[CustomEditor(typeof(SoundManager))]
+[CanEditMultipleObjects]
+public class SoundManagerEditor : Editor {
+
+    public Vector2 metrix;
+    public AudioMixer audioMixer;
+    // Use this for initialization
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        SoundManager sm = (SoundManager)target;
+        sm._audioMixer = EditorGUILayout.ObjectField("AudioMixer", sm._audioMixer, GUILayoutOption,false);
+
+        //sm._audioMixer = EditorGUILayout.IntField("Yolo", sm._audioMixer);
+        if (GUILayout.Button("Add Object"))
+            sm.AddObject();
+
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class SoundManager : MonoBehaviour {
+
+    public static SoundManager instance = null;
+
+    public AudioMixer _audioMixer;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+
+        DontDestroyOnLoad(gameObject);
+
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /*-------------------------------*/
+
+    public void AddObject()
+    {
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.parent = gameObject.transform;
+        GameObject.DestroyImmediate(cube.GetComponent<BoxCollider>());
+    }
+}
