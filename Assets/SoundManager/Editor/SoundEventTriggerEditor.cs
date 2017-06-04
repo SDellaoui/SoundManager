2b5afa1b1a0d8ce2c3e4588d91e1faae6c8393c2
Apo <s.dellaoui@gmail.com>
2017-06-04 23:56:24 +0200

    Add Event Trigger
    Add Event trigger

---
+++ Assets/SoundManager/Editor/SoundEventTriggerEditor.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SoundEventTrigger))]
+[CanEditMultipleObjects]
+public class SoundEventTriggerEditor : Editor {
+
+	public SoundEventTrigger _sndEvtTrigger;
+	public SoundEventData _sndEvtData;
+
+	public int _eventIndex = 0;
+
+	void OnEnable()
+	{
+		/*
+		//_sndEvtData = ScriptableObject.CreateInstance<SoundEventData>();
+		if (_sndEvtData == null)
+			_sndEvtData = Object.FindObjectOfType<SoundEventData>();      
+		// then instantiate if needed
+		if (_sndEvtData == null)
+			_sndEvtData = ScriptableObject.CreateInstance<SoundEventData>();
+		Debug.Log(_sndEvtData._events[0]);
+		*/
+		_sndEvtData = Resources.Load<SoundEventData>("EventsData");
+	}
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		_sndEvtTrigger = (SoundEventTrigger)target;
+		_sndEvtTrigger._events = _sndEvtData._events;
+		_eventIndex = EditorGUILayout.Popup("Events List", _eventIndex, _sndEvtTrigger._events, EditorStyles.popup);
+
+		serializedObject.ApplyModifiedProperties();
+	}
+}
---
+++ Assets/SoundManager/Scripts/SoundEventTrigger.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundEventTrigger : MonoBehaviour {
+
+	public string[] _events;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
